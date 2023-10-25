55a92e5656fca517f54c7bae8d000280ca1c623d
harshithkandula <148646309+harshithkandula@users.noreply.github.com>
2023-10-25 17:46:59 +0530

    Update Zeal.java

--- Zeal.java
+++ Zeal.java
@@ -3,11 +3,6 @@ import java.util.*;
 class Zeal{
     Scanner sc=new Scanner(System.in);
     System.out.println("Enter Zeal Characterstics");
-    String[] a=new String[n];
-    for (int i=0;i<n;i++){
-     a[i]=sc.nextLine();
-      }
-   for(int j=0;j<n;j++){
-   System.out.println(a[j]); 
-   }
+    String a=sc.nextLine();
+    
 }
